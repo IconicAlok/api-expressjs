0ec88045bc728f8300bf2b60ce7d740d4a30246b
alokkuri <alokkuri98@gmail.com>
2026-01-27 11:04:17 +0600

    query parameter

--- src/index.mjs
+++ src/index.mjs
@@ -5,8 +5,13 @@ const PORT = process.env.PORT || 5000;
 
 const mockUsers = [
     { id: 1, userName: "anson", displayName: "Anson" },
-    { id: 2, userName: "jonson", displayName: "Jonson" },
-    { id: 3, userName: "adam", displayName: "Adam" }
+    { id: 2, userName: "jason", displayName: "Jason" },
+    { id: 3, userName: "adam", displayName: "Adam" },
+    { id: 4, userName: "jack", displayName: "Jack" },
+    { id: 5, userName: "tina", displayName: "Tina" },
+    { id: 6, userName: "marilyn", displayName: "Marilyn" },
+    { id: 7, userName: "henry", displayName: "Henry" },
+
 ];
 
 app.get("/", (request, response) => {
@@ -14,7 +19,16 @@ app.get("/", (request, response) => {
 });
 
 app.get("/api/users", (request, response) => {
-    response.send(mockUsers);
+    console.log(request.query);
+    const {
+        query: {filter, value},
+    } = request;
+    //when filter and value are undefined
+    // if(!filter &&  !value) return response.send(mockUsers);
+
+    if(filter && value) return response.send(
+        mockUsers.filter((user)=>user[filter].includes(value))
+    );
 });
 
 app.get("/api/users/:id", (request, response) => {
