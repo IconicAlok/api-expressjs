1441250ff3de3c48a203b209e13fafe66325b3bb
Alok Kuri <alokkuri98@gmail.com>
2026-05-10 02:15:15 +0600

    Cookies

--- src/index.mjs
+++ src/index.mjs
@@ -1,15 +1,17 @@
 import express from "express";
-
 import routes from "./routes/index.mjs";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser("hellowrold"));
 app.use(routes);
 
 app.get("/", (request, response) => {
+  response.cookie("hello", "world", { maxAge: 30000, signed: true });
   response.status(201).send({ msg: "Hello World!" });
 });
 
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -3,7 +3,12 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/api/products", (request, response) => {
-  response.send([{ id: 123, prosucts: "Chicken Breats", price: 12.2 }]);
+  console.log(request.headers.cookie);
+  console.log(request.cookies);
+  console.log(request.signedCookies);
+  if (request.signedCookies.hello && request.signedCookies.hello === "world")
+    response.send([{ id: 123, prosucts: "Chicken Breats", price: 12.2 }]);
+  return response.status(403).send({ msg: "Sorry you need correct cookie" });
 });
 
 export default router;
