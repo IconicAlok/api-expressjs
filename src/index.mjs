6b8849f6cdc5efd24074a955438ecad04fd24f2e
Alok Kuri <alokkuri98@gmail.com>
2026-05-11 01:13:55 +0600

    passport-js authentication

--- src/index.mjs
+++ src/index.mjs
@@ -1,15 +1,31 @@
-import express from "express";
+import express, { request } from "express";
 import routes from "./routes/index.mjs";
 import cookieParser from "cookie-parser";
 import { mockUsers } from "./utils/constants.mjs";
 import session from "express-session";
+import passport from "passport";
+import "./strategies/local-strategies.mjs";
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cookieParser("hellowrold"));
-app.use(session({ secret: "hello", resave: false, saveUninitialized: false }));
+app.use(
+  session({
+    secret: "hello the dev",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 60000 * 3,
+    },
+  }),
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(routes);
 
 app.listen(PORT, () => {
@@ -21,6 +37,26 @@ app.get("/", (request, response) => {
   response.status(201).send({ msg: "Hello World!" });
 });
 
+app.post("/api/auth", passport.authenticate("local"), (request, response) => {
+  response.send(200);
+});
+
+app.get("/api/auth/status", (request, response) => {
+  console.log("inside '/auth/status' API endpoit");
+  console.log(request.user);
+  console.log(request.session);
+  return request.user ? response.send(request.user) : response.sendStatus(401);
+});
+
+app.get("/api/auth/logout", (request, response) => {
+  if (!request.user) return response.sendStatus(401);
+  request.logout((err) => {
+    if (err) return response.sendStatus(400);
+    response.send(200);
+  });
+});
+
+/*
 app.post("/api/auth", (request, response) => {
   const {
     body: { username, password },
@@ -40,6 +76,7 @@ app.get("/api/auth/status", (request, response) => {
     ? response.status(200).send(request.session.user)
     : response.status(401).send({ msg: "Not Authenticated" });
 });
+
 app.post("/api/cart", (request, response) => {
   if (!request.session.user) return response.sendStatus(401);
   const { body: item } = request;
@@ -56,3 +93,4 @@ app.get("/api/cart", (request, response) => {
   if (!request.session.user) return response.sendStatus(401);
   return response.send(request.session.cart ?? []);
 });
+*/
